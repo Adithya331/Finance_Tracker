df6eb417406ea2096e86b7faea513f7a13d5f2bc
Adithya331 <velaparthiadithya@gmail.com>
2024-04-27 03:13:03 +0530

    added currpath log.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,18 @@ import Sidebar from '../components/Sidebar'
 import HomeView from '../components/HomeView'
 
 const Home = () => {
+
+  // added a small functionality to print the current path. we can ad this functionality after adding all the pages 
+
+  // const data = {
+  //   home: 'Home',
+  //   expenses: 'Expenses'
+  // }
+
+  // var curr_pathh = window.location.pathname;
+  // curr_pathh = pathh.substring(1,);
+  // console.log('the path is',data[curr_path]);
+
   return (
     <div className="flex bg-black h-screen w-screen">
       <div className=''>
